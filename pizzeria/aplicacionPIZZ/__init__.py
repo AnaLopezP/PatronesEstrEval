c0f70363d51f6bae08a8c2b237b32b31a41fb6b9
Ana López <anamuyamiga@gmail.com>
2023-11-18 19:31:19 +0100

    rrrr

--- pizzeria/aplicacionPIZZ/__init__.py
+++ pizzeria/aplicacionPIZZ/__init__.py
@@ -1,19 +1,28 @@
 '''Este archivo contiene la inicializacion de la aplicacion y la lógica común que necesita para funcionar.
 Aquí inicializo instancias de clases, configuro bases de datos, etc.'''
-
-from flask import Flask, request
-from pizzeria import Pizzeria_builder as l
+import sys
+sys.path.append('C:/Users/Usuario/Documents/GITHUB2/PatronesEstrEval/pizzeria')
+from flask import Flask, request, render_template
+import Pizzeria_builder.pizzeria_AnaLaRana as l
 #from Pizzeria_builder import pizzeria_AnaLaRana
-from aplicacionPIZZ import routes
+#from aplicacionPIZZ import routes
 
 app = Flask(__name__)
 
 
-pizza_builder = l.Pizza()
-director = l.PizzaDirector(pizza_builder)
 
+@app.route('/home')
+def home():
+    return "HOLA"
+
+@app.route('/pizzacreativa')
+def pizzacreativa():
+    #manejar_formulario()
+    return 'sdas'
+
+@app.route('/formulario', methods=['POST'])
 def manejar_formulario():
-    #recojo los datos del formulario del html
+    '''#recojo los datos del formulario del html
     masa = request.form.get(masa)
     salsa = request.form.get(salsa)
     ingrediente = request.form.get(ingrediente)
@@ -21,10 +30,10 @@ def manejar_formulario():
     presentacion = request.form.get(presentacion)
     extras = request.form.get(extras)
     bebidas = request.form.get(bebidas)
-    postre = request.form.get(postre)
+    postre = request.form.get(postre)'''
     
     #paso los datos al director para que cree la pizza
-    director._builder.crear_masa(masa)
+    '''director._builder.crear_masa(masa)
     director._builder.crear_salsa(salsa)
     director._builder.crear_ingrediente(ingrediente)
     director._builder.crear_tecnica(tecnica)
@@ -32,9 +41,13 @@ def manejar_formulario():
     director._builder.crear_extras(extras)
     director._builder.crear_bebidas(bebidas)
     director._builder.crear_postre(postre)
-    
-    director.crear_pizza()
+    '''
+    #director.crear_pizza()
     return "Pizza pedida con éxito."
     
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+#pizza_builder = l.Pizza()
+#director = l.PizzaDirector(pizza_builder)
--- pizzeria/aplicacionPIZZ/routes.py
+++ pizzeria/aplicacionPIZZ/routes.py
@@ -1,6 +1,6 @@
 '''Este archivo contiene las rutas específicas de la aplicacion.
 Puedo definir las rutas y fnciones asociadas a ellas en este archivo.'''
-
+'''
 from flask import render_template
 from aplicacionPIZZ import app, director
 import __init__ as init
@@ -13,3 +13,4 @@ def home():
 def pizzacreativa():
     init.manejar_formulario()
     return render_template('creatupizza.html')
+'''
