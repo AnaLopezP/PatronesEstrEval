e312ee0f5b5991675dbc48df1e548676cbfc2522
Ana López <anamuyamiga@gmail.com>
2023-11-19 14:30:49 +0100

    de momento lo dejo así

--- pizzeria/aplicacionPIZZ/Pizzeria_builder/cliente.py
+++ pizzeria/aplicacionPIZZ/Pizzeria_builder/cliente.py
@@ -3,12 +3,3 @@ class Usuario():
         self.nombre = nombre
         self.direccion = direccion
         self.ordenes = []
-
-    '''def pedido_actual(self, pizza):
-        self.ordenes.append(pizza)
-        
-    def ultimo_pedido(self):
-        if self.ordenes:
-            return self.ordenes[-1]
-        else:
-            return "No hay pedidos"'''
