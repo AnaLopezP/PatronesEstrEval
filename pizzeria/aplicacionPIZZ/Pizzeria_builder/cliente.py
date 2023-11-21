bde7bdcba6676a172cb20fd15737306a7e792f18
Ana López <anamuyamiga@gmail.com>
2023-11-21 13:16:31 +0100

    sdjd

--- pizzeria/aplicacionPIZZ/Pizzeria_builder/cliente.py
+++ pizzeria/aplicacionPIZZ/Pizzeria_builder/cliente.py
@@ -1,3 +1,5 @@
+import csv
+import os
 class Usuario():
     def __init__(self, nombre, direccion, usuario, contraseña, telefono, email):
         self.nombre = nombre
@@ -7,5 +9,44 @@ class Usuario():
         self.telefono = telefono
         self.email = email
         self.ordenes = []
-        
+
+    def registrar_usuario(usuario):
+        # Nombre del archivo CSV
+        archivo_csv = 'usuarios.csv'
+
+        # Verificar si el archivo CSV existe
+        file_exists = os.path.isfile(archivo_csv)
+
+        with open(archivo_csv, mode='a', newline='') as file:
+            fieldnames = ['nombre', 'direccion', 'usuario', 'contraseña', 'telefono', 'email']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+            if not file_exists:
+                writer.writeheader()  # Escribir encabezados solo si el archivo no existe
+
+            # Escribir datos del usuario en el archivo CSV
+            writer.writerow({
+                'nombre': usuario.nombre,
+                'direccion': usuario.direccion,
+                'usuario': usuario.usuario,
+                'contraseña': usuario.contraseña,
+                'telefono': usuario.telefono,
+                'email': usuario.email
+            })
+
+    def iniciar_sesion(nombre_usuario, contraseña):
+        # Nombre del archivo CSV
+        archivo_csv = 'usuarios.csv'
+
+        with open(archivo_csv, mode='r') as file:
+            reader = csv.DictReader(file)
+
+            # Verificar las credenciales del usuario
+            for row in reader:
+                if row['usuario'] == nombre_usuario and row['contraseña'] == contraseña:
+                    return True
+
+        return False
+
+            
 
